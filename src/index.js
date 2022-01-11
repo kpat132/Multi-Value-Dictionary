46839455146f03314426f7bc1ee00b4d646c6b1b
Krishna <krishna@talageins.com>
2022-01-11 15:26:25 -0800

    KEYS and MEMBERS functionality

--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@ const readline = require('readline');
 const readlineInterface = readline.createInterface(process.stdin, process.stdout);
 
 const dictionary = new Map();
-
+let index = 0;
+let memberArray = [];
+let count = 1;
 console.log('Welcome to the Dictionary app. \nPlease choose a command (Or I for options):');
 
 let cli = function () {
 
     readlineInterface.question('> ', userInput => {
         userInput = userInput.split(" ");
+        let key = userInput[1];
 
         switch (userInput[0].toUpperCase()) {
 
             case 'KEYS':
+                keys(userInput);
                 break;
 
             case 'MEMBERS':
+                members(userInput,key);
                 break;
 
             case 'ADD':
@@ -59,6 +64,38 @@ let cli = function () {
 
 cli();
 
+function keys(userInput){
+    count = 1;
+
+    if (dictionary.size === 0) {
+        console.log('(empty set)');
+    }
+    if (userInput.length > 1) {
+        console.log("ERROR, Too many input values.");
+    } else {
+        for (let key of dictionary.keys()) {
+            console.log(`${count}) ` + key);
+            count++;
+        }
+    }
+}
+
+function members(userInput,key){
+    if (userInput.length !== 2) {
+        console.log("ERROR: Input must include command and one key.")
+    }
+    if (dictionary.has(key)) {
+        for (const member of dictionary.get(key)) {
+            console.log(member);
+        }
+
+    } else {
+        console.log('ERROR, key does not exist.');
+    }
+}
+
 module.exports = {
-    cli
+    cli,
+    keys,
+    members
 }
